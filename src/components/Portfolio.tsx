0632f21a2c1f0614dfa30a7db23aac8d53d4a424
tjjoris <tjjoris@gmail.com>
2026-05-23 18:50:16 -0400

    made filter for projects.json and projects.json

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,11 +5,35 @@ import aScienceOfRealmsImage from "../assets/images/science-of-realms-cover-imag
 import RoundedButton from "./RoundedButton";
 import NewWindowRoundedButton from "./NewWindowRoundedButton";
 import ProjectBar from "./ProjectBar";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import projectsJson from "../assets/data/projects.json";
 
 
 export default function Portfolio() {
+
+    type ProjectData = {
+        name: string;
+        description: string;
+        demo: string | null;
+        image: string;
+        moreInfo: string;
+        gitHub: string;
+        categories: string[];
+    }
     const [category, setCategory] = useState("React/Front-end");
+    // const filteredProjects = useRef<ProjectData[]>([]);
+    const filteredProjects = projectsJson.projects.filter((project) =>
+        project.categories.includes(category));
+
+
+    // useEffect(() => {
+    //     filteredProjects.current = [];
+    //     projectsJson.projects.forEach((element) => {
+    //         if (element.categories.includes(category)) { filteredProjects.current.push(element) }
+    //     }
+
+    //     )
+    // }, [category])
 
     const fuzzyKittensInfoButton = () => {
         return (
@@ -30,6 +54,7 @@ export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
             {category}
+            {filteredProjects.length}
             <ProjectBar setCategory={setCategory} />
             <Project imageUrl={loomByLightImage} title="Loom by Light" description="LBL project." backgroundColor="rgb(80, 80, 178)" DemoButton={lBlDemoButton} LearnMoreButton={lblInfoButton} />
             <Project imageUrl={fuzzyKittensImage} title="Fuzzy Kittens" description="A cool project." backgroundColor="rgb(76, 76, 179)" DemoButton={null} LearnMoreButton={fuzzyKittensInfoButton} />
