9502fe99e128892d6eb78defaa836e168c565021
tjjoris <tjjoris@gmail.com>
2026-05-23 16:08:02 -0400

    qualifications grid with 2 rows.

--- src/components/Qualification.tsx
+++ src/components/Qualification.tsx
@@ -1,8 +1,8 @@
-
+import "../assets/css/qualifications.css"
 export default function Qualification({ name, description, iconUrl }: { name: string, description: string, iconUrl: string }) {
     return (
         <section className="qualification">
-            <img src={iconUrl} alt={name} className={"qualification-icon"} />
+            <img src={iconUrl} alt={name} className="qualification-icon" />
             <div className="qualification-info">
                 <h3>{name}</h3>
                 <p>{description}</p>
