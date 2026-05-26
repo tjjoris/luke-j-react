54e74228cfaa8809db7573f741e77d68c77c0173
tjjoris <tjjoris@gmail.com>
2026-05-26 12:51:30 -0400

    removed unused code from Portfolio.tsx and commended out code not being used.

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,27 +1,15 @@
 import Project from "./Project";
 import ProjectBar from "./ProjectBar";
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import projectsJson from "../assets/data/projects.json";
 import ActionRoundedButton from "./ActionRoundedButton";
-import { type LearnMore } from "../types/learnMore";
 import { openLinkInNewTab } from "../functions/openLinkInNewTab";
-import { useContext } from "react";
-import { PopupContext } from "../contexts/popupContext";
-import { type ProjectData } from "../types/projectData";
+// import { useContext } from "react";
+// import { PopupContext } from "../contexts/popupContext";
 
 export default function Portfolio() {
 
-    // type ProjectData = {
-    //     name: string;
-    //     description: string;
-    //     demo: string | null;
-    //     image: string;
-    //     learnMore: LearnMore | null;
-    //     gitHub: string;
-    //     categories: string[];
-    // }
-    //popup context
-    const popupContext = useContext(PopupContext);
+    // const popupContext = useContext(PopupContext);
 
     const [category, setCategory] = useState("React/Front-end");
     const filteredProjects = projectsJson.projects.filter((project) =>
@@ -54,10 +42,6 @@ export default function Portfolio() {
                             LearnMore={project.learnMore} />
                     )
                 })}
-            {/* })} */}
-            {/* <Project imageUrl={loomByLightImage} title="Loom by Light" description="LBL project." backgroundColor="rgb(80, 80, 178)" DemoButton={lBlDemoButton} LearnMoreButton={lblInfoButton} />
-            <Project imageUrl={fuzzyKittensImage} title="Fuzzy Kittens" description="A cool project." backgroundColor="rgb(76, 76, 179)" DemoButton={null} LearnMoreButton={fuzzyKittensInfoButton} />
-            <Project imageUrl={aScienceOfRealmsImage} title="A Science of Realms" description="Award winning game" backgroundColor="rgb(72, 72, 160)" DemoButton={null} LearnMoreButton={null} /> */}
         </div>
     )
 }
